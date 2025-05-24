c1176b48dafc635cd34fe3f18c44ac157aa2db7f
101-sachin <yadavsachin6987@gmail.com>
2025-05-24 13:24:37 +0530

    project api added

--- api/projects.js
+++ api/projects.js
@@ -4,14 +4,14 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const [result] = await db.query("select * projects");
+    const [result] = await db.query("select * from projects");
 
     if (!result || result.length === 0) {
       return res.status(404).json({ error: "Project Not Found" });
     }
     return res.status(200).json(result)
   } catch (error) {
-    return res.status(500).json({ error: "Server error while fetching profile data" });
+    return res.status(500).json({ error: "Server error while fetching Project data" });
   }
 });
 
